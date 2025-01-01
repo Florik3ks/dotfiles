479bdb7abb61186620bda3ac9e1a47d7c58757c0
flori <floritrs@gmail.com>
2025-01-02 00:20:13 +0100

    remove waybar as it is no longer needed

--- waybar/scripts/gpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-utilization=$(nvidia-smi --query-gpu utilization.gpu --format=csv,noheader 2> /dev/null)
-temp=$(nvidia-smi --query-gpu temperature.gpu --format=csv,noheader 2> /dev/null)
-
-
-cut=$(echo ${utilization::-1})
-echo  $cut% GPU
